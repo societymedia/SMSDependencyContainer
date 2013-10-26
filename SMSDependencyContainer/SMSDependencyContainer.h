089dd942ce449ac3a03d00aff4c27498418d4563
Tony Merante <tony.merante@bottlerocketapps.com>
2013-10-26 13:52:46 -0500

    initial commit

---
+++ SMSDependencyContainer/SMSDependencyContainer.h
@@ -0,0 +1,13 @@
+//
+//  SMSDependencyContainer.h
+//  SMSDependencyContainer
+//
+//  Created by Tony Merante on 10/26/13.
+//  Copyright (c) 2013 Societymedia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMSDependencyContainer : NSObject
+
+@end
---
+++ SMSDependencyContainer/SMSDependencyContainer.m
@@ -0,0 +1,13 @@
+//
+//  SMSDependencyContainer.m
+//  SMSDependencyContainer
+//
+//  Created by Tony Merante on 10/26/13.
+//  Copyright (c) 2013 Societymedia. All rights reserved.
+//
+
+#import "SMSDependencyContainer.h"
+
+@implementation SMSDependencyContainer
+
+@end
