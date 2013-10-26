585bc30d41db2bdf506f7555214ab4e982602ec3
Tony Merante <tony.merante@bottlerocketapps.com>
2013-10-26 14:30:19 -0500

    adding initial code

--- SMSDependencyContainer/SMSDependencyContainer.h
+++ SMSDependencyContainer/SMSDependencyContainer.h
@@ -8,6 +8,16 @@
 
 #import <Foundation/Foundation.h>
 
-@interface SMSDependencyContainer : NSObject
+typedef NS_ENUM(NSInteger, SMSDependencyContainerScope) {
+    SMSDependencyContainerScopeSingleton     = 0,
+    SMSDependencyContainerScopeTransient     = 1
+};
 
+typedef id (^SMSDependencyContainerHandler)(void);
+
+@interface SMSDependencyContainer : NSObject
++ (instancetype)sharedInstance;
+- (id)resolve:(Protocol *)protocol;
+- (void)bindToProtocol:(Protocol *)protocol withClass:(Class)class inScope:(SMSDependencyContainerScope)scope;
+//- (void)initWithBindings:(NSDictionary *)bindings;
 @end
--- SMSDependencyContainer/SMSDependencyContainer.m
+++ SMSDependencyContainer/SMSDependencyContainer.m
@@ -8,6 +8,74 @@
 
 #import "SMSDependencyContainer.h"
 
+
+@interface SMSDependencyContainer()
+@property (nonatomic, strong) NSMutableDictionary *container;
+@end
+
 @implementation SMSDependencyContainer
+typedef id (^SMSInstanceHandler)(void);
+
+
+- (id)init
+{
+    self = [super init];
+    if (self)
+    {
+       self.container =  [NSMutableDictionary dictionary];
+    }
+
+    return self;
+}
+
+
+
+
+
++ (instancetype)sharedInstance
+{
+    static dispatch_once_t once;
+    static SMSDependencyContainer *sharedInstance;
+    dispatch_once(&once, ^{
+        sharedInstance = [[self alloc] init];
+    });
+    return sharedInstance;
+}
+
+
+- (id)resolve:(Protocol *)protocol
+{
+    SMSDependencyContainerHandler block = [self.container objectForKey:NSStringFromProtocol(protocol)];
+    return block();
+}
+
+- (void)bindToProtocol:(Protocol *)protocol withClass:(Class)class inScope:(SMSDependencyContainerScope)scope
+{
+    NSAssert([class conformsToProtocol:protocol], @"class %@ does not conform to protocol %@", NSStringFromClass(class), NSStringFromProtocol(protocol));
+
+    id object;
+    if(scope == SMSDependencyContainerScopeSingleton){
+        object = [[class alloc]init];
+    }
+
+    if([class conformsToProtocol:protocol]){
+        [self.container setObject: ^id {
+
+            if(scope == SMSDependencyContainerScopeSingleton){
+                return object;
+            }
+
+            return [[class alloc]init];
+        } forKey:NSStringFromProtocol(protocol)];
+    }
+}
+
+
+- (void)addObject:(id)instance {
+    if(![self.container objectForKey:instance]){
+        [self.container setObject:instance forKey:instance];
+    }
+}
+
 
 @end
